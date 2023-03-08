64f4681b0f6e7b738bfbc488ccfa6d2ea1fde1ca
giabertu <giabertu99@gmail.com>
2023-03-08 16:04:11 +0000

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Gianni Bertuzzi on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
