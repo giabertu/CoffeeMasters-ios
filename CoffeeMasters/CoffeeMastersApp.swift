8b90e576d4053f10bcd4339b6aeac09da697c510
giabertu <giabertu99@gmail.com>
2023-03-11 12:36:05 +0000

    first commit

--- CoffeeMasters/CoffeeMastersApp.swift
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct CoffeeMastersApp: App {
+    //a scene is kinda like a window
+    //having more than one scene means that on iPad you can have two windows of this app open at the same time.
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+ 
--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        EmptyView()
     }
 }
+
+
+
+//struct Greeting: View {
+//    //this is equivalent to a state variable
+//    @State var name = "unnamed"
+//    var body: some View{
+//        VStack {
+//            //the dollar sign is used for double binding -- you read and write
+//            TextField("Enter your name: ", text: $name)
+//            //no dollar sign because you only read the value
+//            Text("Hello \(name)")
+//        }
+//    }
+//}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+// vstack takes in a closure -- this is trailing closure syntax!
+// the HStack that you see below is the initialiser of the HStack struct
+// You can omit the closure from the arguments passed (if its the last one)
+// And then just simply open curly brackets. This is what we are doing here!
+// In js it would be new HStack(() => {...code µ})
